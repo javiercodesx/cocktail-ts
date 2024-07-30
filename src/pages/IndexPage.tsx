899a774609292cd479a777a7954074f99f511714
javierspr <learn.javier@gmail.com>
2024-07-30 11:25:37 -0300

    add zustand with slice pattern

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,5 +1,11 @@
+import { useAppStore } from "../stores/useAppStore"
 
 export default function IndexPage() {
+
+  const { categories } = useAppStore()
+  console.log(categories);
+  
+
   return (
     <>
         <h1>Inicio</h1>
