a26e1e0dd9ce37532e31d114e5dc0fc64b4c76e2
javierspr <learn.javier@gmail.com>
2024-07-30 10:58:39 -0300

    add header form

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,76 @@
-import { NavLink } from "react-router-dom"
+import { useMemo } from "react"
+import { NavLink, useLocation } from "react-router-dom"
 
 export default function Header() {
-  return (
-    <header className="bg-slate-800">
-        <div className="mx-auto container px-5 py-16">
-            <div className="flex justify-between items-center">
-                <div>
-                    <img className="w-32" src="/logo.svg" alt="logo" />
+
+    const { pathname } = useLocation()
+    const isHome = useMemo(() => pathname === '/', [pathname])
+
+    return (
+        <header className="bg-slate-800">
+            <div className="mx-auto container px-5 py-12">
+                <div className="flex justify-between items-center">
+                    <div>
+                        <img className="w-32" src="/logo.svg" alt="logo" />
+                    </div>
+
+                    <nav className="flex gap-5">
+                        <NavLink 
+
+                            to='/'
+                            className={({isActive}) => 
+                                isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold' 
+                            }
+                        >Inicio</NavLink>
+                        <NavLink 
+                            to='/favoritos'
+                            className={({isActive}) => 
+                                isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold' 
+                            }
+                        >Favoritos</NavLink>
+                    </nav>
                 </div>
 
-                <nav className="flex gap-5">
-                    <NavLink 
+                { isHome && (
+                    <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-24 p-10 rounded-lg shadow space-y-6">
+                        <div className="space-y-4">
+                            <label 
+                                htmlFor="ingredient"
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >Nombre o Ingredientes</label>
 
-                        to='/'
-                        className={({isActive}) => 
-                            isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold' 
-                        }
-                    >Inicio</NavLink>
-                    <NavLink 
-                        to='/favoritos'
-                        className={({isActive}) => 
-                            isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold' 
-                        }
-                    >Favoritos</NavLink>
-                </nav>
+                            <input 
+                                type="text" 
+                                id="ingredient" 
+                                name="ingredient" 
+                                className="p-3 w-full rounded-lg"
+                                placeholder="Nombre o Ingrediente. Ej. Tequila, Vodka..."
+                            />
+                        </div>
+                        <div className="space-y-4">
+                            <label 
+                                htmlFor="ingredient"
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >Categoría</label>
+
+                            <select 
+                                id="ingredient" 
+                                name="ingredient" 
+                                className="p-3 w-full rounded-lg"
+                            >
+                                <option value="">-- Seleccione --</option>
+
+                            </select>
+                        </div>
+
+                        <input 
+                            type="submit" 
+                            value='Buscar Recetas' 
+                            className="cursor-pointer bg-orange-700 hover:bg-orange-800 w-full rounded-lg p-2 uppercase" 
+                        />
+                    </form>
+                )}
             </div>
-        </div>
-    </header>
+        </header>
   )
 }
