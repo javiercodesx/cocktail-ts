0c1f90abce51e14d8edda71ee900bebb23056d29
javierspr <learn.javier@gmail.com>
2024-07-31 23:55:54 -0300

    add selectedRecipe state

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
-import { CategoriesAPISchema, Drink, Drinks, SearchFilterSchema } from "../utils/recipes-schema";
+import { CategoriesAPISchema, Drink, Drinks, Recipe, SearchFilterSchema } from "../utils/recipes-schema";
 
 export type Categories = z.infer<typeof CategoriesAPISchema>
 export type SearchFilter = z.infer<typeof SearchFilterSchema>
 export type Drink = z.infer<typeof Drink>
 export type Drinks = z.infer<typeof Drinks>
+export type Recipe = z.infer<typeof Recipe>
